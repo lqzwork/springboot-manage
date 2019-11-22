1e0d72baf775091072b8ea6ca1e2240497c5d0e6
liqingzheng <liqz32@chinaunicom.cn>
2019-11-22 13:46:18 +0800

    规格、单位

--- src/main/java/com/jesper/model/Item.java
+++ src/main/java/com/jesper/model/Item.java
@@ -9,13 +9,17 @@ public class Item extends BaseObject{
     private int id;
     private String title;
     private String sellPoint;
+    private String norm;
+    private String unit;
     private int price;
     private int num;
     private String barcode;
     private String image;
     private int cid;
     private int status;
+    private String createUserId;
     private Date created;
+    private String updteUserId;
     private Date updated;
     private String createdStr;
     private String updatedStr;
