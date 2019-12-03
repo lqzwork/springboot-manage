6fb4ba212ec98792160ed0268ec67fb8fd01194b
liqingzheng <liqz32@chinaunicom.cn>
2019-12-03 15:31:54 +0800

    数据库初始化

---
+++ src/main/resources/db/product_baixin.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost-liqz
+ Source Server Type    : MySQL
+ Source Server Version : 50646
+ Source Host           : localhost:3306
+ Source Schema         : product_baixin
+
+ Target Server Type    : MySQL
+ Target Server Version : 50646
+ File Encoding         : 65001
+
+ Date: 03/12/2019 15:31:34
+*/
+
